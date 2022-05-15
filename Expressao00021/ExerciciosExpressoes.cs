486d4c2cb71d2691ca82ae6f5bde9c4660c0e1b1
alberto <albertobarbosa0003@gmail.com>
2022-05-15 12:29:19 -0300

    Adicionar arquivos de projeto.

---
+++ Expressao00021/ExerciciosExpressoes.cs
@@ -0,0 +1,17 @@
+﻿namespace Expressao00021;
+
+public class ExercicicosExpressoes
+{
+    public static void Main(string[] args)
+    {
+        //[(18 + 3 · 2) ÷ 8 + 5 · 3] ÷ 6.
+        int a = (((18 + 3 * 2) / 8) + 5 * 3) / 6;
+        Console.WriteLine(a);
+
+        //{[(8 · 4 + 3) ÷ 7 + (3 + 15 ÷ 5) · 3] · 2 – (19 – 7) ÷ 6} · 2 + 12.
+        int b = (((8 * 4 + 3) / 7 + (3 + 15 / 5) * 3) * 2 - (19 - 7) / 6) * 2 + 12;
+        Console.WriteLine(b);
+
+
+    }
+}
